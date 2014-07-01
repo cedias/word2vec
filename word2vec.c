b1af06a6530a1fe1bb8dc07920f2ebdfc587a49d
Charles-Emmanuel Dias <Charles-Emmanuel.Dias@etu.upmc.fr>
2014-07-01 18:39:53 +0200

    [wip] vocab.h, remake the vocabulary, act 1

--- word2vec.c
+++ word2vec.c
@@ -27,6 +27,8 @@
 #include <math.h>
 #include <pthread.h>
 
+#include "vocab.h"
+
 #define MAX_STRING 100
 #define EXP_TABLE_SIZE 1000
 #define MAX_EXP 6
@@ -34,15 +36,7 @@
 #define MAX_CODE_LENGTH 40
 
 
-const int vocab_hash_size = 30000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
-
 typedef float real;                    // Precision of float numbers
-
-struct vocab_word {
- 	long long cn; //times of occurence in train file
- 	int *point;
- 	char *word, *code, codelen;
-};
 
 struct save_vec_struct{
 	char *word;
@@ -51,10 +45,10 @@ struct save_vec_struct{
 
 char train_file[MAX_STRING], output_file[MAX_STRING];
 char save_vocab_file[MAX_STRING], read_vocab_file[MAX_STRING];
-struct vocab_word *vocab;
+
 int binary = 0, cbow = 0, debug_mode = 2, window = 5, min_count = 5, num_threads = 1, min_reduce = 1, ngram = 0, hashbang = 0, group_vec = 0;
 int double_train = 0; int dtinit = 0;
-int *vocab_hash;
+
 long long vocab_max_size = 1000, vocab_size = 0, layer1_size = 100;
 long long train_words = 0, word_count_actual = 0, file_size = 0, classes = 0;
 real alpha = 0.025, starting_alpha, sample = 0;
@@ -62,6 +56,8 @@ real alpha = 0.025, starting_alpha, sample = 0;
 real *syn0, *syn1, *syn1neg, *expTable;
 save_vec* saveArray;
 long long unsigned int saveArrayLength = 0;
+
+vocabulary *vocab;
 
 clock_t start;
 
@@ -95,95 +91,6 @@ void InitUnigramTable() {
 		}
 }
 
-// Reads a single word from a file, assuming space + tab + EOL to be word boundaries
-void ReadWord(char *word, FILE *fin) {
-	int a = 0, character;
-	
-	while (!feof(fin)) {
-		character = fgetc(fin);
-
-
-
-		if (character == 13) //Carriage Return
-			continue;
-
-		if ((character == ' ') || (character == '\t') || (character == '\n')) {
-			
-			if (a > 0) {
-
-		    	if (character == '\n')
-		    		ungetc(character, fin); //we don't want the new line char.
-		    break;
-		  	}
-
-		 	if (character == '\n') { 
-			    strcpy(word, (char *)"</s>");  //newline become </s> in corpus
-			    return;
-		  	}
-		 	else
-		  		continue;
-		}
-
-		word[a] = character;
-		a++;
-
-		if (a >= MAX_STRING - 1)
-			a--;   // Truncate too long words
-	}
-
-	word[a] = '\0';
-
-	if(hashbang > 0)
-	{
-
-		a = strlen(word); //'\0'
-		word[a] = '#';
-		a++;
-		word[a] = '\0';
-		a++;
-
-
- 		while(a>0)
- 		{
- 			word[a] = word[a-1];
- 			a--;
- 		}
-
- 		word[0] ='#';
-	}
-
-
-	return;
-}
-
-// Returns hash value of a word
-int GetWordHash(char *word) {
-	unsigned long long a, hash = 0;
-
-	for (a = 0; a < strlen(word); a++) 
-		hash = hash * 257 + word[a];
-
-	hash = hash % vocab_hash_size;
-	return hash;
-}
-
-void DestroyVocab() {
-  int a;
-
-  for (a = 0; a < vocab_size; a++) {
-    if (vocab[a].word != NULL) {
-      free(vocab[a].word);
-    }
-    if (vocab[a].code != NULL) {
-      free(vocab[a].code);
-    }
-    if (vocab[a].point != NULL) {
-      free(vocab[a].point);
-    }
-  }
-  free(vocab[vocab_size].word);
-  free(vocab);
-}
 
 void DestroyNet() {
   if (syn0 != NULL) {
@@ -197,160 +104,6 @@ void DestroyNet() {
   }
 }
 
-// Returns position of a word in the vocabulary; if the word is not found, returns -1
-int SearchVocab(char *word) {
-	unsigned int hash = GetWordHash(word);
-
-	while (1) {
-		if (vocab_hash[hash] == -1)
-			return -1;
-
-		if (!strcmp(word, vocab[vocab_hash[hash]].word))
-			return vocab_hash[hash];
-
-		hash = (hash + 1) % vocab_hash_size;
-	}
-
-	return -1;
-}
-
-// Reads a word and returns its index in the vocabulary
-int ReadWordIndex(FILE *fin) {
-	char word[MAX_STRING];
-	ReadWord(word, fin);
-
-	if (feof(fin)) 
-		return -1;
-
-	return SearchVocab(word);
-}
-
-// Adds a word to the vocabulary
-int AddWordToVocab(char *word) {
-
-	unsigned int hash, length = strlen(word) + 1;
-
-	if (length > MAX_STRING)
-		length = MAX_STRING;
-
-		
-	vocab[vocab_size].word = (char *)calloc(length, sizeof(char));
-
-	strcpy(vocab[vocab_size].word, word);
-
-	
-
-	vocab[vocab_size].cn = 0;
-	vocab_size++;
-
-	// Reallocate memory if needed
-	if (vocab_size + 2 >= vocab_max_size) {
-
-		
-		vocab_max_size += 1000;
-
-		vocab = (struct vocab_word *)realloc(vocab, vocab_max_size * sizeof(struct vocab_word));
-		
-	}
-	
-	hash = GetWordHash(word);
-
-	while (vocab_hash[hash] != -1)
-		hash = (hash + 1) % vocab_hash_size;
-
-	vocab_hash[hash] = vocab_size - 1;
-	
-	return vocab_size - 1;
-}
-
-// Used later for sorting by word counts
-int VocabCompare(const void *a, const void *b) {
-    return ((struct vocab_word *)b)->cn - ((struct vocab_word *)a)->cn;
-}
-
-// Sorts the vocabulary by frequency using word counts
-void SortVocab() {
-	int a, size;
-	unsigned int hash;
-
-	if(debug_mode > 2)
-		printf("Sorting Vocab...\n");
-
-	// Sort the vocabulary and keep </s> at the first position
-	qsort(&vocab[1], vocab_size - 1, sizeof(struct vocab_word), VocabCompare);
-
-	for (a = 0; a < vocab_hash_size; a++)
-		vocab_hash[a] = -1;
-
-	size = vocab_size;
-	train_words = 0;
-
-	for (a = 1; a < size; a++) {
-	// Words occuring less than min_count times will be discarded from the vocab
-		if (vocab[a].cn < min_count) {
-			vocab_size--;
-			//free(vocab[vocab_size].word); 
-			//free(vocab[a].word);
-			vocab[a].word = NULL;
-		}
-		else {
-		// Hash will be re-computed, as after the sorting it is not actual
-			hash = GetWordHash(vocab[a].word);
-
-			while (vocab_hash[hash] != -1)
-				hash = (hash + 1) % vocab_hash_size;
-
-			vocab_hash[hash] = a;
-			train_words += vocab[a].cn;
-		}
-	}
-
-
-	vocab = (struct vocab_word *)realloc(vocab, (vocab_size + 1) * sizeof(struct vocab_word));
-
-	// Allocate memory for the binary tree construction
-	for (a = 0; a < vocab_size; a++) {
-		vocab[a].code = (char *)calloc(MAX_CODE_LENGTH, sizeof(char));
-		vocab[a].point = (int *)calloc(MAX_CODE_LENGTH, sizeof(int));
-	}
-
-}
-
-// Reduces the vocabulary by removing infrequent tokens
-void ReduceVocab() {
-
-	int a, b = 0;
-	unsigned int hash;
-
-	for (a = 0; a < vocab_size; a++){
-		if (vocab[a].cn > min_reduce) {
-
-			vocab[b].cn = vocab[a].cn;
-			vocab[b].word = vocab[a].word;
-			b++;
-
-		} else
-			free(vocab[a].word);
-	}
-
-	vocab_size = b;
-
-	for (a = 0; a < vocab_hash_size; a++)
-		vocab_hash[a] = -1;
-
-	for (a = 0; a < vocab_size; a++) {
-		// Hash will be re-computed, as it is not actual
-		hash = GetWordHash(vocab[a].word);
-
-		while (vocab_hash[hash] != -1)
-			hash = (hash + 1) % vocab_hash_size;
-
-		vocab_hash[hash] = a;
-	}
-
-	fflush(stdout);
-	min_reduce++;
-}
 
 // Create binary Huffman tree using the word counts
 // Frequent words will have short uniqe binary codes
@@ -436,159 +189,6 @@ void CreateBinaryTree() {
 	free(parent_node);
 }
 
-//Look if word already in vocab, if not add, if yes, increment.
-void searchAndAddToVocab(char* word){
-	long long a,i;
-	i = SearchVocab(word);
-
-		if (i == -1) {
-			a = AddWordToVocab(word);
-			vocab[a].cn = 1;
-		} else
-			vocab[i].cn++;
-
-		if (vocab_size > vocab_hash_size * 0.7)
-			ReduceVocab();
-}
-
-void LearnVocabFromTrainFile() {
-	char word[MAX_STRING];
-	FILE *fin;
-	int i,start,end,lenWord;
-
-	char gram[ngram+1];
-
-	for (i = 0; i < vocab_hash_size; i++) //init vocab hashtable
-		vocab_hash[i] = -1;
-
-	fin = fopen(train_file, "rb");
-
-	if (fin == NULL) {
-		printf("ERROR: training data file not found!\n");
-		exit(1);
-	}
-	
-	vocab_size = 0;
-	AddWordToVocab((char *)"</s>");
-
-	while (1) {
-		ReadWord(word, fin);
-
-		if(ngram > 0) //learn ngrams instead of words
-		{
-			lenWord = strlen(word);
-
-			if(lenWord<=ngram){ //word smaller or equal to ngram var.
-				searchAndAddToVocab(word);
-
-				if (feof(fin))
-					break;
-				else
-					continue;
-			}
-
- 			start = 0;
-			end = ngram-1;
-			i=0;
-
-			while(end<lenWord)
-			{
-
-				for (i = 0; i < ngram; i++)
-				{
-					gram[i] = word[start+i];
-				}
-				gram[ngram] = '\0';
-
-				searchAndAddToVocab(gram);
-
-				end++;
-				start++;
-			}
-		}
-		else
-		{
-			searchAndAddToVocab(word);
-		}
-
-		if (feof(fin))
-			break;
-
-		train_words++;
-
-		
-
-		if ((debug_mode > 1) && (train_words % 100000 == 0)) {
-			printf("%lldK%c", train_words / 1000, 13);
-			fflush(stdout);
-		}
-	}
-
-	SortVocab();
-
-	if (debug_mode > 1) {
-		printf("Vocab size: %lld\n", vocab_size);
-		printf("Words in train file: %lld\n", train_words);
-	}
-
-	file_size = ftell(fin);
-	fclose(fin);
-}
-
-void SaveVocab() {
-  long long i;
-  FILE *fo = fopen(save_vocab_file, "wb");
-
-  for (i = 0; i < vocab_size; i++)
-  	fprintf(fo, "%s %lld\n", vocab[i].word, vocab[i].cn);
-
-  fclose(fo);
-}
-
-void ReadVocab() {
-	long long a, i = 0;
-	char c;
-	char word[MAX_STRING];
-	FILE *fin = fopen(read_vocab_file, "rb");
-
-	if (fin == NULL) {
-		printf("Vocabulary file not found\n");
-		exit(1);
-	}
-
-	for (a = 0; a < vocab_hash_size; a++)
-		vocab_hash[a] = -1;
-
-	vocab_size = 0;
-
-	while (1) {
-		ReadWord(word, fin);
-
-		if (feof(fin))
-			break;
-
-		a = AddWordToVocab(word);
-		fscanf(fin, "%lld%c", &vocab[a].cn, &c);
-		i++;
-	}
-
-	SortVocab();
-
-	if (debug_mode > 1) {
-		printf("Vocab size: %lld\n", vocab_size);
-		printf("Words in train file: %lld\n", train_words);
-	}
-	fin = fopen(train_file, "rb");
-
-	if (fin == NULL) {
-		printf("ERROR: training data file not found!\n");
-		exit(1);
-	}
-
-	fseek(fin, 0, SEEK_END);
-	file_size = ftell(fin);
-	fclose(fin);
-}
 
 void InitNet() {
 	long long a, b;
@@ -1008,12 +608,12 @@ void TrainModel() {
 	starting_alpha = alpha;
 
 	if (read_vocab_file[0] != 0)
-		ReadVocab();
+		vocab = ReadVocab();
 	else
-		LearnVocabFromTrainFile();
+		vocab = LearnVocabFromTrainFile();
 
 	if (save_vocab_file[0] != 0)
-		SaveVocab();
+		SaveVocab(vocab);
 
 	if (output_file[0] == 0) //quit
 		return;
@@ -1569,10 +1169,6 @@ int main(int argc, char **argv) {
 	if ((i = ArgPos ((char *) "-dt", argc, argv)) > 0 ) double_train = atoi(argv[i + 1]);
 	if ((i = ArgPos ((char *) "-dtinit", argc, argv)) > 0 ) dtinit = atoi(argv[i + 1]);
 	
-
-	vocab = (struct vocab_word *)calloc(vocab_max_size, sizeof(struct vocab_word));
-
-	vocab_hash = (int *)calloc(vocab_hash_size, sizeof(int));
 	expTable = (real *)malloc((EXP_TABLE_SIZE + 1) * sizeof(real));
 
 	for (i = 0; i < EXP_TABLE_SIZE; i++) {
@@ -1585,7 +1181,6 @@ int main(int argc, char **argv) {
 	if(ngram > 0 && double_train == 0){
 		printf("Creating word vectors.\n");
 		createWordVector(1);
-
 	}
 
 
