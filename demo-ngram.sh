0b901e12441e33ef83ee184691a38baa8c20e2b7
Charles-Emmanuel <Charles-Emmanuel.Dias@etu.upmc.fr>
2017-01-12 17:02:33 +0100

    Update demo-ngram.sh

--- demo-ngram.sh
+++ demo-ngram.sh
@@ -3,5 +3,5 @@ if [ ! -e text8 ]; then
   wget http://mattmahoney.net/dc/text8.zip -O text8.gz
   gzip -d text8.gz -f
 fi
-time ./word2gram -train text8 -output vectors.bin -ngram 2 -group -1 -hashbang 1 -cbow 0 -size 200 -window 10 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1 -min-count 0
+time ./word2gram -train text8 -output vectors.bin -ngram 2 -group 0 -hashbang 1 -cbow 0 -size 200 -window 10 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1 -min-count 0
 ./distance vectors.bin
